a91cba6c54e1c9334c5792d9fc34ae368f15e780
Rauan Caracciolo <rauancaracciolo@gmail.com>
2025-07-07 11:18:39 -0300

    First commit

---
+++ MoneyAPI/src/java/model/Transaction.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+	private Double value;
+	private String desc;
+	private String cat;
+	private LocalDateTime time;
+}
